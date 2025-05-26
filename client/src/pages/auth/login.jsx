d4e73f8437fee4f6d7f4f1498a503d0fb6cb5465
Clement <kwasiclement764@gmail.com>
2025-05-26 03:47:04 +0000

    commit

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -25,6 +25,7 @@ function AuthLogin() {
           title: data?.payload?.message,
         });
       } else {
+        console.log(data);
         toast({
           title: data?.payload?.message,
           variant: "destructive",
@@ -40,7 +41,7 @@ function AuthLogin() {
           Sign in to your account
         </h1>
         <p className="mt-2">
-          Don't have an account
+          Don&apos;t have an account
           <Link
             className="font-medium ml-2 text-primary hover:underline"
             to="/auth/register"

c9c41c8ddb9ba9812d3929805d6bfb335745cba9
Clement <kwasiclement764@gmail.com>
2025-05-28 05:47:26 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["https://ecommerce-client-mx55.onrender.com", "http://localhost:5174"],
+    origin: ["https://ecommerce-client-mx55.onrender.com", "http://localhost:5174", "http://localhost:5173"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
